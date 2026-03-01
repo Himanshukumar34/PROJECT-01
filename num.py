b486eb94421e7a6c11cae78a6e1b5eaec5755086
himanshu rajput <gourirajput553@gmail.com>
2026-03-01 23:11:19 +0530

    Create num.py

---
+++ num.py
@@ -0,0 +1,23 @@
+# num1=int(input("Enter your num1: "))
+# if num1<0:
+#     print("number is negative ")
+# elif num1>0:
+#     print("number is positive ")
+# else:
+#     print("number is zero")
+
+# for i in range(11,0,-1):
+#     print(i)
+# Print numbers from 1 to 20 using while loop.
+# count=0
+# while True:
+#     count+=1
+#     print(count)
+#     if count==20:
+#         break
+count=0
+while  count<=20:
+    count+=1
+    if count==count%2:
+        print(count)
+
