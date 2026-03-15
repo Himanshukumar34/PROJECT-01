870cd8adf32f098242f81157be26a510ad102e48
himanshu kumar <gourirajput553@gmail.com>
2026-03-15 22:11:44 +0530

    num1

--- new2.py
+++ new2.py
@@ -1,13 +1,24 @@
-maths_marks=int(input("write your maths_marks"))
-evs_marks=int(input("write your evs_marks"))
-sst_marks=int(input("write your sst_makrs"))
-gk_marks=int(input("write your gk_marks"))
-hindi_marks=int(input("write your hindi_marks"))
+# maths_marks=int(input("write your maths_marks"))
+# evs_marks=int(input("write your evs_marks"))
+# sst_marks=int(input("write your sst_makrs"))
+# gk_marks=int(input("write your gk_marks"))
+# hindi_marks=int(input("write your hindi_marks"))
 
 
 
-total_marks=maths_marks + evs_marks + sst_marks + gk_marks + hindi_marks
-average_marks=total_marks/5
+# total_marks=maths_marks + evs_marks + sst_marks + gk_marks + hindi_marks
+# average_marks=total_marks/5
 
 
-print("average_marks=",average_marks)
+# print("average_marks=",average_marks)
+
+import random
+choice=input("Enter your num1: ")
+count=[]
+while True:
+    num=random.randint(1,100)
+    count.append(num)
+
+    print(count)
+
+
