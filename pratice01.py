8e7304b206481b177c72584ae4bee46f478f9dcf
himanshu kumar <gourirajput553@gmail.com>
2026-04-15 17:17:34 +0530

    add some changes

--- pratice01.py
+++ pratice01.py
@@ -1,62 +1,101 @@
-# flow of control 
-# Q.1 program that inputs three numbers and calculates two suma as per this .
-# sum1 as the sum of all input numbers 
-# sum2 as the sum of non duplicate  numbers if there are duplicatees numbers in the input ignores them ?
-# a=int(input("Enter your num1 :"))
-# b=int(input("Enter your num1 :"))
-# c=int(input("Enter your num1 :"))
-# sum1=a+b+c
-# print("sum of the given three number are ",a+b+c)
-# sum2=0
-# if a!=b and a!=c:
-#     sum2+=a
-# if  b!=c and b!=a:
-#     sum2+=b
-# if c!=a and c!=b:
-#     sum2+=c
-# print("sum of non duplicate numbers is ", sum2)
+# # flow of control 
+
+# EXAMPLES
+# # Q.1 program that inputs three numbers and calculates two suma as per this .
+# # # sum1 as the sum of all input numbers 
+# # # sum2 as the sum of non duplicate  numbers if there are duplicatees numbers in the input ignores them ?
+# # a=int(input("Enter your num1 :"))
+# # b=int(input("Enter your num1 :"))
+# # c=int(input("Enter your num1 :"))
+# # sum1=a+b+c
+# # print("sum of the given three number are ",a+b+c)
+# # sum2=0
+# # if a!=b and a!=c:
+# #     sum2+=a
+# # if  b!=c and b!=a:
+# #     sum2+=b
+# # if c!=a and c!=b:
+# #     sum2+=c
+# # print("sum of non duplicate numbers is ", sum2)
 
 
-# Q.2 program to find the multiples of a numbers the divisor out of the given five numbers?
-# a=int(input("Enter your num1 :"))
-# b=int(input("Enter your num1 :"))
-# c=int(input("Enter your num1 :"))
-# d=int(input("Enter your num1 :"))
-# e=int(input("Enter your num1 :"))
-# divisor =input("Enter your num1 :")
-# if a%divisor == 0:
-#     print(f",Multiple of {divisor } is {a}")
-# if b%divisor == 0:
-#     print(f",Multiple of {divisor } is {b}")
-# if c%divisor == 0:
-#     print(f",Multiple of {divisor } is {c}")
-# if d%divisor == 0:
-#     print(f",Multiple of {divisor } is {d}")
-# if e%divisor == 0:
-#     print(f",Multiple of {divisor } is {e}")
+# # Q.2 program to find the multiples of a numbers the divisor out of the given five numbers?
+# # a=int(input("Enter your num1 :"))
+# # b=int(input("Enter your num1 :"))
+# # c=int(input("Enter your num1 :"))
+# # d=int(input("Enter your num1 :"))
+# # e=int(input("Enter your num1 :"))
+# # divisor =input("Enter your num1 :")
+# # if a%divisor == 0:
+# #     print(f",Multiple of {divisor } is {a}")
+# # if b%divisor == 0:
+# #     print(f",Multiple of {divisor } is {b}")
+# # if c%divisor == 0:
+# #     print(f",Multiple of {divisor } is {c}")
+# # if d%divisor == 0:
+# #     print(f",Multiple of {divisor } is {d}")
+# # if e%divisor == 0:
+# #     print(f",Multiple of {divisor } is {e}")
 
-# Q.3 program to prints the sum of natural numbers betweemn 1 to 7 . print the sum progressively i.e after adding each natural number , print sum so far ?
+# # Q.3 program to prints the sum of natural numbers betweemn 1 to 7 . print the sum progressively i.e after adding each natural number , print sum so far ?
+# # count=0
+# # for j  in range(1,8):
+# #     count+=j
+# #     print("sum of natural number 0 to 7 ",count)
+
+
+# # Q.4 program to calculate the foactorial of a number ?
+# num1=int(input("Enter your num1: "))
 # count=0
-# for j  in range(1,8):
-#     count+=j
-#     print("sum of natural number 0 to 7 ",count)
+# for i in range(1,num1+1):
+#     num1*num1
 
-
-# Q.4 program to calculate the foactorial of a number ?
-num1=int(input("Enter your num1: "))
-count=0
-for i in range(1,num1+1):
-    num1*num1
-
-# Q.5 program to calculate and print the sums of evena dn odd intergers of the first n natural numbers ?
+# Q.5 program to calculate and print the sums of even  and  odd intergers of the first n natural numbers ?
+# num1 = int(input("Enter your natural number : "))
+# even=0
+# odd=0
+# for i in range(0,num1+1):
+#     num2=i%2
+#     if num2== 0 :
+#         even+=num2
+#     elif num2!=0:
+#         odd+=num2
+# print("The sum of even integers is ", even)
+# print("The sum of odd integers is " , odd )
 
 # Q.6 program to input some numbers repeaditly and print their sum . till the use not say  no to enter more numbers ?
+# count=0
+# while True:
+#     user=str(input("want to enter more numbers :"))
+#     if user=="yes":
+#           num1=int(input("Enter number :"))
+#           count+=num1
+#     elif user=="no":
+#         print("your total sum is ",count)
+#         break
+
+# Q.7 program to input a number and test if its a prime number ?
+# num1=int(input("Enter your number :"))
+# num2=num1%1 
+# num3=num1%num1
+# if num2==0 and num3 == 0:
+#     print("IT is a prime number ")
+# else:
+#     print("It is not a prime number ")
 
 
-# Q.7 program to input a numbers and test if its a prime numbers ?
-+
+# # NCERT GUIDLINES 
+# # Q.1 write a program that prints minimum and maximum of five numbers eneterd by the user?
+# a=int(input("Enter your num1 :"))
+# b=int(input("Enter your num1 :"))
+# c=int(input("Enter your num1 :")) 
+# d=int(input("Enter your num1 :")) 
+# e=int(input("Enter your num1 :")) 
+# max, min= 
+# Q.2 Write a progam to check if the year entered by the user is a leap ?
 
-    
+# Q.3 write a program to find the sum of digit of an integers number , input by the user? 
+num1=int(input("Enter your numbers :"))
 
 
 
