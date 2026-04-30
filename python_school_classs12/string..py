24b68c225b8541d20a670ebce445a4f4f171adf1
himanshu kumar <gourirajput553@gmail.com>
2026-04-30 15:34:05 +0530

    add some files

--- python_school_classs12/string..py
+++ python_school_classs12/string..py
@@ -1,4 +1,10 @@
 #                                         string pratice problem
 
 # Q.1 program to read a string and display it in reverse order display one charcter per line do not create a reverse string , just display in revrse 
-order
+# order?
+strings1= input("Enter your strings : ")
+length=len(strings1)
+for i in range(-1,-length,-1):
+      print(strings1[i])
+          
+          
