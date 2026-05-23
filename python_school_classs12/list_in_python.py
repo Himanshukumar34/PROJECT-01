774ad17f352baeed53789699488a03b5c9090a30
himanshu kumar <gourirajput553@gmail.com>
2026-05-23 23:02:15 +0530

    Doing some questions

--- python_school_classs12/list_in_python.py
+++ python_school_classs12/list_in_python.py
@@ -12,3 +12,36 @@
 # # output is False
 # 35 not in L
 # # output is false
+
+# SOLVED EXAMPLES
+# Q.1 Program to print elements of a list[q,w,e,r,t,y] in sepreate lines aloong with elements both indexes ( positive and negative indexes)?
+# num1=['q','w','e','r','t','y']
+# for i in range(len(num1)):
+#     print(f"At indexes {i} and {i-(len(num1))} elements : {num1[i]}")
+
+# Q.2 Extract two list slices out of a given list of numbers . Display and print the sum of elements of first list slices which conatins evary other elements of the list between 5 to 15 
+# . Program should also display the average of elements in second list slice that conatins every fouth elements of the list . The given list conatins numbers from 1 to 20.
+# L1= [1,2,3,4,5,6,7.8,9,10,11,12,13,14,15,16,17,18,19,20]
+# slice1=L1[5:15]
+# slice2=L1[::4]
+# print(slice2)
+
+# Q.3 Write a program to create a copy of a list . In the list is copy , add 10 to its first and last elements . Then display the lists ?
+# L1=[34,65,45,33,66,35,36,68]
+# L2=L1
+# count=[]
+# for i in L1:
+#     if L1[0]==i:
+#         i+=10
+#         count.append(i)
+#     elif L1[7]==i:
+#         i+=10
+#         count.append(i)
+#     else:
+#         count.append(i)
+
+# print("Original list ",L1)
+# print("Copy list ",count)
+
+# Q.4 Write a program that asks the user to input a number  a list to be appended to an exisiting list . Wether the user enters a single number or a list of numbers , the program 
+# should append the list accordingly ?
