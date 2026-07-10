6e3fe183bda6e991fce55e4c3052bb23f0bea014
himanshu kumar <gourirajput553@gmail.com>
2026-07-10 23:17:23 +0530

    add some

---
+++ class12/Working_with_function.py
@@ -0,0 +1,31 @@
+"""Simple notes on Python functions for Class 12 students.
+
+What is a function?
+- A function is a named block of code. Use `def` to make one.
+
+How to write a function
+- `def name(parameters):` then write the code indented below.
+- Use `return` to send back a result.
+
+Easy examples
+def greet(name):
+	"""Return greeting for the given name."""
+	return "Hello, " + name
+
+def add(a, b=0):
+	"""Return sum of a and b. b is optional."""
+	return a + b
+
+def sum_all(*nums):
+	"""Return total of all numbers given."""
+	total = 0
+	for n in nums:
+		total += n
+	return total
+
+if __name__ == "__main__":
+	print(greet("World"))
+	print(add(5))
+	print(add(2, 3))
+	print(sum_all(1, 2, 3, 4))
+"""
